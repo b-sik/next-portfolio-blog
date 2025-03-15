a6b037df6e549febb0bf410e028c258bf9ab9175
b-sik <brian@bsik.net>
2025-03-15 09:53:47 -0400

    add local fonts

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: ["./app/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                b612mono: ["B612 Mono", "normal"],
+                "glass-tty": ["Glass TTY VT220", "monospace"],
+                "roboto-mono": ["Roboto Mono", "monospace"],
+            },
+        },
+    },
+    plugins: [],
+};
