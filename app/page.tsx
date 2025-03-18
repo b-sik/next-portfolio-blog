85ace69c91c61ff75413ac01ba9d5c107e911275
b-sik <brian@bsik.net>
2025-03-18 18:01:46 -0400

    contact icons, header and footer layout styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,7 @@ import ProjectCards from "./components/project-cards";
 export default function Home() {
     return (
         <main className="w-full py-8 px-6">
-            <section id="about" className="py-8 mx-auto max-w-4xl ">
-                <h3>About</h3>
-                <p>
-                    Results-driven Frontend Developer with 4+ years of
-                    experience in building responsive, high-performance websites
-                    and applications. Proficient in React, WordPress,
-                    JavaScript, PHP, CSS, and modern frontend frameworks, with a
-                    strong understanding of REST APIs, GraphQL, and full-stack
-                    development.
-                </p>
-            </section>
-            <section id="projects" className="my-8 mx-auto max-w-4xl">
+            <section id="projects" className="py-8 mx-auto max-w-4xl">
                 <h3>Projects</h3>
                 <p>
                     A sample of personal projects. Please see resume for
